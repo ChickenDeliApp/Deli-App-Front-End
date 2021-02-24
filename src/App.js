191320d296caa1054faaf31d32ed40ad54f6c150
Miłosz Milewski <MMilosz122@gmail.com>
2021-02-24 13:20:20 +0000

    add a navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import HomePage from './pages/homepage';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
@@ -13,13 +12,24 @@ function App() {
      //nav bar means user doesnt have to enter the specific url
     }
     <div className="App">
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="/">
+          Chicken App
+        </Navbar.Brand>
+
+        <Nav>
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/register">Delis</Nav.Link>
+          <Nav.Link href="/login">Search</Nav.Link>
+        </Nav>
+      </Navbar>
 
       <Switch>
         {
         // this is how when we navigate to the required function on the app
         //that it returns the function of what we want to do
         }
-        <Route path='/home' component={HomePage} exact />
+        <Route path='/(home|)' component={HomePage} exact />
         <Route path='/register' component={RegisterPage} exact />
         <Route path='/login' component={LoginPage} exact />
        
