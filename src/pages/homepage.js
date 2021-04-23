546ec99049431584f27ab08294d98c4fb06f61a8
CiaranRoche203 <rocheciaran37@gmail.com>
2021-04-23 01:32:47 +0100

    home page updated

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,57 +1,93 @@
-import { Component } from "react";
-import { Card, Col, Container, Pagination, Row } from "react-bootstrap";
+import { Component, useCallback, useEffect, useState } from "react";
+import { Card, Col, Container, Pagination, Row, Table } from "react-bootstrap";
 import ReviewCard from "../components/review_card";
 import $ from "jquery"
-class HomePage extends Component {
+import { useHistory } from "react-router";
+function HomePage() {
 
-	state = {
-		latest: null
-	}
+	const [latestReviews, setLatest] = useState( [] )
+	const [bestReviews, setBest] = useState( [] )
+	const history = useHistory()
 
-	componentDidMount(){
-		let latestReviews = []
+	const fetchBest = useCallback( () => {
+		let bestReviews = []
 
-		$.ajax({
-			url: "/reviews/latest",
-			success: (data) => {
-				data.forEach(review => {
-					latestReviews.push( <ReviewCard date={review.createDate} author={review["User.username"]} store={review.restaurant} rating={review.rating} description={review.text}  ></ReviewCard> )
-				});
+		$.ajax( {
+			url: "/reviews/topdelis",
+			success: ( data ) => {
+				console.log( data );
 
-				this.setState({
-					state: latestReviews
-				})
+				bestReviews = data.map( (review, index) => {
+					return (
+						<tr onClick={() => { history.push(`/restaurant/view/${review.id}`)}}>
+							<td>{index + 1}</td>
+							<td>{review.restaurantName}</td>
+							<td>{review.DeliChainChainName}</td>
+							<td>{parseFloat(review.average).toFixed(1)} (Based on {review.reviews} review)</td>
+						</tr>
+					)
+				} )
+
+				setBest( bestReviews )
 			}
-		})
-		
-	}
+		} )
 
-    render() {
+	}, [] )
 
-        return (
-            <Container fluid>
-                <Row style={{marginTop:"1rem"}}>
-                    <Col>
-                        <h2 className="mb-3 mt-3 text-center">TOP 10 Reviewed Delis (Monthly)</h2>
-						
-                    </Col>
-                
-                    <Col>
-                        <h2 className="mb-3 mt-3 text-center">Newest Reviews</h2>
+	const fetchLatest = useCallback( () => {
+		$.ajax( {
+			url: "/reviews/latest",
+			success: ( data ) => {
+				let latestReviews = data.map( review => {
+					return (
+						<ReviewCard key={review.id} date={review.createDate} author={review?.User?.username} store={review.DeliRestaurant.restaurantName + " || " + review.DeliRestaurant.DeliChainChainName} rating={review.rating} description={review.text}  ></ReviewCard>
+					)
+				} ) ?? []
 
-                        <div className="text-center">
-                            {this.state.latest !== null ? this.state.latest : <small>No reviews to display :(</small>}
-                        </div>
-                    </Col>
+				setLatest( latestReviews )
+			}
+		} )
+	}, [] )
 
-                    <Col>
-                    </Col>
-                </Row>
+	useEffect( () => {
+		fetchLatest()
+		fetchBest()
+	}, [] )
 
+	return (
+		<Container className="">
+			<Row className="justify-center">
+				<Col>
+					<h2 className="mt-3 text-center">TOP 10 Reviewed Delis (Monthly)</h2>
 
-            </Container>
-        )
-    }
+					<Card>
+						<Table bordered striped hover size="sm" className="mb-0">
+							<thead>
+								<tr className="text-center">
+									<th>Rank</th>
+									<th>Restaurant Name</th>
+									<th>Chain</th>
+									<th>Rating</th>
+								</tr>
+							</thead>
+
+							<tbody className="text-center">
+								{bestReviews}
+							</tbody>
+						</Table>
+					</Card>
+				</Col>
+
+				<Col>
+					<h2 className="mt-3 text-center">Newest Reviews</h2>
+
+					<div className="text-center">
+						{latestReviews.length > 0 ? latestReviews : <small>No reviews to display :(</small>}
+					</div>
+				</Col>
+			</Row>
+		</Container>
+	)
 }
 
 export default HomePage;
