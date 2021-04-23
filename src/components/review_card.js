52c838c2db6c1494dc005771f7c3e63db2efa796
CiaranRoche203 <rocheciaran37@gmail.com>
2021-04-23 01:33:11 +0100

    review card changes

--- src/components/review_card.js
+++ src/components/review_card.js
@@ -3,27 +3,21 @@ import { Card } from "react-bootstrap";
 
 class ReviewCard extends Component {
 
-	state = {
-		author: undefined,
-		store: "Undefined Store",
-		rating: 5,
-		date: new Date(),
-		description: undefined
-	}
-
 	render() {
+		let date = new Date(this.props.date)
+
 		return (
-			<Card className="w-75 mx-auto mb-4 text-left">
+			<Card id={this.props.id} className="w-75 mx-auto mb-4 text-left">
                 <Card.Img style={{maxHeight: "7rem", objectFit:"cover"}} src="https://www.rowanscatering.ie/wp-content/uploads/2020/03/deli-1200x800.jpg"></Card.Img>
 
                 <Card.Body>
-                    <Card.Title>{ this.state.store }</Card.Title>
-                    <Card.Subtitle>Review by {this.state.author || "an Anonymous User"} • {this.state.rating ?? 0} stars</Card.Subtitle>
+                    <Card.Title>{ this.props.store }</Card.Title>
+                    <Card.Subtitle>Review by {this.props.author || "an Anonymous User"} • {this.props.rating ?? 0} stars</Card.Subtitle>
 
-					{ this.state.description ? <Card.Text className="mt-4">{ this.state.description }</Card.Text> : null }
+					{ this.props.description ? <Card.Text className="mt-3">{ this.props.description }</Card.Text> : null }
                 </Card.Body>
 
-                <Card.Footer className="text-left text-muted">{this.state.date.toLocaleDateString()}</Card.Footer>
+                <Card.Footer className="text-left text-muted">{date.toLocaleDateString()}</Card.Footer>
             </Card>
 		)
 	}
